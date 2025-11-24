f0fee717514840714542bdbba9b2b8094ac2835a
SilverRAN <silver.weihang@gmail.com>
2025-11-24 11:51:45 +0900

    Fix training voxel bugs

--- run_scripts/train_clean_voxel.sh
+++ run_scripts/train_clean_voxel.sh
@@ -7,6 +7,6 @@ echo "Training scene: ${scene}"
 
 python train_sh_based_voxel_grid_with_posed_images.py \
 	-d ./data/${scene}/ \
-	-o logs/${scene}/ \
+	-o logs/${scene}/ref/ \
 	--fast_debug_mode=True \
 	--sh_degree=0
